0b4e4122ef9c211a54423c6d8b3d52219e16008f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-10 23:13:10 +0600

    hi

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,6 +5,8 @@ const About = () => {
         <div>
             <h1>About me</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis laudantium non quas fugit ipsa repudiandae cumque? Quam earum, voluptatibus dolore ipsum adipisci laboriosam pariatur, architecto modi debitis minus harum.</p>
+            <h1>About me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis laudantium non quas fugit ipsa repudiandae cumque? Quam earum, voluptatibus dolore ipsum adipisci laboriosam pariatur, architecto modi debitis minus harum.</p>
         </div>
     );
 };
