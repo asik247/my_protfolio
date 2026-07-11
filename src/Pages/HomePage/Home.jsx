b6f9035bb6132530b738b661ae2ebf8b508809ae
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-11 23:58:02 +0600

    tody some code added

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
            <h1>Hello Brother this is home pages</h1>
+           <p className='text-2xl font-bold text-green-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor quis reprehenderit optio dignissimos numquam? Cumque eum rerum consectetur animi sint distinctio ullam, tempore numquam explicabo culpa rem, excepturi beatae eaque?</p>
         </div>
     );
 };
