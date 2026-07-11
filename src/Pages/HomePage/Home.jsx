946bbd424de815b59a4474a2aef2b51d76dae5fc
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-11 23:57:30 +0600

    tody some code added

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>HOme</h1>
-            <h1>HOme</h1>
+           <h1>Hello Brother this is home pages</h1>
         </div>
     );
 };
