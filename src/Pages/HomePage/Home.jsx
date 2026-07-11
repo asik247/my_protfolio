acd1d6a237bdc456bd3e4c9c03fc7580485edc50
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-12 00:08:14 +0600

    now about,home page code change

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
            <h1>Hello Brother this is home pages</h1>
-           
+            <About></About>
         </div>
     );
 };
