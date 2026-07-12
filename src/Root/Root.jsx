41d0ddd09ad9804e7577e360f46cd22cca37eec4
Asikur Rahaman <mdasikur5893@gmail.com>
2026-07-12 22:33:31 +0600

    nav desing

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
-        <div>
+        <div className='w-full max-w-7xl mx-auto p-4'>
             <Nav></Nav>
              <div>
                 <Outlet></Outlet>
